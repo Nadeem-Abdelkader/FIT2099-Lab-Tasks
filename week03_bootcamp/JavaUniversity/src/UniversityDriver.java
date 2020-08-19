9f2f3532c199934d5cece882889b30bc8b4eda16
Nadeem <nabd0018@student.monash.edu>
2020-08-19 18:30:36 +0200

    WEEK 2 BOOTCAMP

--- week03_bootcamp/JavaUniversity/src/UniversityDriver.java
+++ week03_bootcamp/JavaUniversity/src/UniversityDriver.java
@@ -1,3 +1,8 @@
+/*
+Nadeem Emadeldin Hamed Hamed Abdelkader
+30146224
+Lab: Friday 3 PM
+ */
 public class UniversityDriver {
     public static void main(String[] args) {
         University newUni = new University();
