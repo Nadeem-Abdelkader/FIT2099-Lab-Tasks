f39807fd6b8f97c2008f4e90bc3318553aad839c
Nadeem <nabd0018@student.monash.edu>
2020-09-17 20:11:25 +0200

    WEEK 6 BOOTCAMP

--- week06_bootcamp/JavaUniversity/src/Mark.java
+++ week06_bootcamp/JavaUniversity/src/Mark.java
@@ -1,43 +1,68 @@
+/**
+ * This class is instantiated for every student that is enrolled and it is unit specific
+ */
 public class Mark{
 
     private int markTotal;
     private String comment;
 
+    /**
+     * constructor for Mark class
+     * @param newMarkTotal - new Mark
+     * @param newComment - comment on the new mark
+     * @throws Exception - if newMarkTotal is greater than 100 or less than 1
+     */
     public Mark(int newMarkTotal, String newComment) throws Exception {
-        if (newMarkTotal < 1) {
-            throw new Exception("Mark too low");
-        }
-        if (newMarkTotal > 100) {
-            throw new Exception("Mark too high");
-        }
         setMarkTotal(newMarkTotal);
         setComment(newComment);
     }
 
+    /**
+     * constructor for Mark class
+     * @param newMarkTotal - new Mark
+     * @throws Exception - if newMarkTotal is greater than 100 or less than 1
+     */
     public Mark(int newMarkTotal) throws Exception {
-        if (newMarkTotal < 1) {
-            throw new Exception("Mark too low");
-        }
-        if (newMarkTotal > 100) {
-            throw new Exception("Mark too high");
-        }
         setMarkTotal(newMarkTotal);
     }
 
+    /**
+     * getter function for markTotal
+     * @return markTotal
+     */
     public int getMarkTotal()
     {
         return markTotal;
     }
 
-    public void setMarkTotal(int newMarks){
+    /**
+     * setter function for markTotal
+     * @param newMarks
+     * @throws Exception - if newMarks is greater than 100 or less than 1
+     */
+    public void setMarkTotal(int newMarks) throws Exception {
+        if (newMarks < 1) {
+            throw new Exception("Mark too low");
+        }
+        if (newMarks > 100) {
+            throw new Exception("Mark too high");
+        }
         markTotal = newMarks;
     }
 
+    /**
+     * getter function for comment
+     * @return comment
+     */
     public String getComment()
     {
         return comment;
     }
 
+    /**
+     * setter function for comment
+     * @param newComment
+     */
     public void setComment(String newComment)
     {
         comment = newComment;
